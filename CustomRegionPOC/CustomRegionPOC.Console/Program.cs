a5ef878e36b2776ec5b1e59ccd4279c8f58dcb31
Hussain Ali Akbar <hussain94aa@gmail.com>
2018-05-17 13:40:46 +0500

    added console project for data migration

---
+++ CustomRegionPOC/CustomRegionPOC.Console/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Amazon;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2.Model;
+using Amazon.Runtime;
+using CustomRegionPOC.Common.Model;
+using CustomRegionPOC.Common.Service;
+using IronPython.Hosting;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Linq;
+using Amazon.DynamoDBv2.DocumentModel;
+using System.Drawing;
+using System.Net;
+using System.IO;
+using CustomRegionPOC.Common.Helper;
+
+namespace CustomRegionPOC.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
